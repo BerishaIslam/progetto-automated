7348601e3a228ecafae11c690317e81c9ca63e73
BerishaIslam <berishaislam7@gmail.com>
2025-01-31 11:12:28 +0100

    Updated checkstyle file

--- src/main/java/it/islam/automated/AutomatedApplication.java
+++ src/main/java/it/islam/automated/AutomatedApplication.java
@@ -5,11 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
-
 public class AutomatedApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(AutomatedApplication.class, args);
 	}
-
 }
